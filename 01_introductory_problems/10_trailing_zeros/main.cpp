15e2cabf0c97f928a35fe18c228b92e879b40f64
MevyDev <mevydev@proton.me>
2025-12-25 01:16:55 +0100

    trailing zeros solved

---
+++ 01_introductory_problems/10_trailing_zeros/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+#define debug(x) cerr << #x << R"(: )" << x << '\n';
+#define REP(i,a) for (int i = 0; i < a; ++i)
+#define FILL(a) for (auto &A : a) cin >> A;
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ins insert
+#define fi first
+#define se second
+
+void solve() {
+    // base number that has a zero is 10
+    // factors of 10 are 2 and 5
+    // there will always be more factors 2 than 5 in any factorial
+    // therefore the number of 10s we can build is bounded by
+    // the count of factors 5
+    ll n; cin >> n;
+    ll res = 0;
+    for (ll k = 5; k <= n; k *= 5) {
+        if (k > n) break;
+        res += n/k;
+    }
+    cout << res << '\n';
+}
+
+signed main() {
+    // ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+}
