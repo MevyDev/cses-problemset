04e2376ca262fd8636676a4abb86366267ce162b
MevyDev <mevydev@proton.me>
2026-03-25 00:55:43 +0100

    template

---
+++ template.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+
+#include <sstream>
+#include <string>
+
+#include <array>
+#include <bitset>
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <limits>
+#include <numeric>
+#include <utility>
+
+// #define int long long
+
+using namespace std;
+using ll = long long;
+using vb = vector<bool>;
+using vi = vector<int>;
+using vpi = vector<pair<int, int>>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
+using vvl = vector<vector<ll>>;
+#define debug(x) cerr << #x << R"(: )" << x << '\n';
+#define rep(i, a) for (int i = 0; i < a; ++i)
+#define fill(a) for (auto &A : a) cin >> A;
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ins insert
+#define fi first
+#define se second
+int test_n;
+
+void solve() {
+
+}
+
+signed main() {
+    // ios::sync_with_stdio(false); cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        test_n++;
+        solve();
+    }
+}
